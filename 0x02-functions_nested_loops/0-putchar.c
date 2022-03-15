cd40cdeea51578fdbf6944b5370d86c5435b1521
Mike15254 <mike8max10@gmail.com>
2022-03-15 17:37:01 +0300

    Getting there

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,15 @@
 
 
 int main(void) {
-        int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
+int str[] = {95, 112, 117, 116, 99, 72, 97, 144};
+int count, sz;
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
+{
+_putchar(str[count]);
+}
+_putchar('\n');
+return (0);
 	
 }
 
