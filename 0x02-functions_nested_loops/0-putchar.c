c737e1d54439b38a94956fde37567e3719ceca17
Mike15254 <mike8max10@gmail.com>
2022-03-15 16:30:07 +0300

    Geting better

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+
 
 int main () {
 
 	char _putchar;
 	putchar(_putchar);
 
-	return 0;
+	return(0);
 }
