16101db4b0fffd902ad054eed72e1c040d58a531
Mike15254 <mike8max10@gmail.com>
2022-03-17 23:27:45 +0300

    Its working!..nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,34 +4,31 @@
  * main - prints Buzz each numbers of 3 and 5.
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int n;
 
-	n=1;
-
+	n = 1;
 	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d", n);
+			printf(" %d", n);
 		}
-		printf("\n");
-		return (0);
 	}
+	printf("\n");
+	return (0);
 }
-
